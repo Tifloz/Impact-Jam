a5f512e9db483aeadde89501ce72c3cb8a74bc69
Alexis Leclercq <alexis1.leclercq@epitech.eu>
2019-03-20 10:51:47 +0100

    [+] Added Over script

---
+++ Assets/Scripts/Over.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Over : MonoBehaviour
+{
+    private int objects = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        objects++;
+        Debug.Log(objects);
+    }
+}
